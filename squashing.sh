14a6c0b72bc16b43a32c534b04e34b412e8f5570
isanstrazimiri <isanstrazimiri@gmail.com>
2021-06-01 12:10:07 +0200

    updated fetch:

--- squashing.sh
+++ squashing.sh
@@ -4,8 +4,8 @@ set -xe
 
 git config --global user.email ${INPUT_USER_EMAIL}
 git config --global user.name ${INPUT_USER_NAME}
-git checkout master
 git fetch
+git checkout master
 msg=$(git log --oneline --format=%B -n 1 HEAD | head -n 1)
 git checkout ${INPUT_TARGET_BRANCH}
 git reset --soft HEAD~${INPUT_NO_COMMITS} && git commit -m "$msg" && git push https://${{ github.token }}@github.com/${INPUT_REPO_NAME}.git --force
